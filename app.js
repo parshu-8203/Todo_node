cd39e92af2215d5557df95e0411a1803ce89971d
parashuram yerranagu <parshu.yerranagu@gmail.com>
2022-09-09 17:42:23 +0530

    git ingore file added

--- app.js
+++ app.js
@@ -160,7 +160,12 @@ app.post("/", function (req, res) {
             }
         });
     }
-})
-app.listen(3000, function () {
+}) 
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
+
+app.listen(port, function () {
     console.log("Server is started at port 3000");
 })
